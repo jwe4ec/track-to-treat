c56e9d0469ee8c996975319a488b30fcd5daee1e
Jeremy Eberle <jwe4ec@virginia.edu>
2025-04-17 15:01:14 -0500

    Note that it is unclear why some IDs are missing

--- Qualtrics Data Cleaning Helper Functions.R
+++ Qualtrics Data Cleaning Helper Functions.R
@@ -6,7 +6,7 @@
 # Function to drop invalid responses
 remove_invalid_responses <- function(data, id) {
   
-  # Taking the data, filter out cases where the ID variable is missing or in invalid_ids
+  # Taking the data, filter out cases where ID variable is missing (unclear why) or in invalid_ids
   out <- data %>%
     filter(
       !is.na({{id}}),
