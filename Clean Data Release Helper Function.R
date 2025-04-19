0c62416a09d04c218f586cf11d0e4deffb1ef358
Jeremy Eberle <jwe4ec@virginia.edu>
2025-04-19 11:36:15 -0500

    Condense create_data_release() and script

--- Clean Data Release Helper Function.R
+++ Clean Data Release Helper Function.R
@@ -1,7 +1,7 @@
 #### Helper function to create versioned clean data release ####
 
 create_data_release <- function(clean_data_staging_dir, clean_data_final_dir, staged_filenames) {
-  ### Load staged data and logs into named list
+  ### Load staged files into named list
   
   staged_files <- lapply(paste0(clean_data_staging_dir, "\\", staged_filenames), readRDS)
   names(staged_files) <- staged_filenames
@@ -9,22 +9,20 @@ create_data_release <- function(clean_data_staging_dir, clean_data_final_dir, st
   ### Obtain version info from user via console (preventing storage of info in script, 
   ### which would risk overwriting files) and obtain date from system. After obtaining
   ### user's confirmation via console to create versioned folder containing versioned
-  ### files, do so. User can press ESC to exit loops designed to obtain user input.
+  ### files, do so. User can press ESC to exit loops for obtaining user input.
   
   ## Obtain version info from user via console and ensure correct format
-  
-  version_firstname <- ""
   
   repeat {
     ver_prompt <- "Enter a version number and your first name in this format (v0.1_Jeremy): "
     version_firstname <- readline(ver_prompt)
     
-    if (version_firstname == "" | !grepl("v", version_firstname) | !grepl("_", version_firstname)) {
-      cat("You must provide a response in the requested format! Press ESC to stop.\n")
+    if (!grepl("v", version_firstname) | !grepl("_", version_firstname)) {
+      cat("You must provide a response in the requested format! Press ESC to stop.")
     } else break
   }
   
-  cat("Valid 'version_firstname' entered:", version_firstname, "\n")
+  cat("Valid 'version_firstname' entered:", version_firstname)
   
   ## Obtain date this script was run from system
   
@@ -32,19 +30,18 @@ create_data_release <- function(clean_data_staging_dir, clean_data_final_dir, st
   
   ## Obtain date cleaning code was last modified from user via console and ensure format
   
-  cleaning_code_date <- ""
-  
   repeat {
     code_date_prompt <- "Enter the date the cleaning code was last modified in this format (YYYY-MM-DD): "
     cleaning_code_date <- readline(code_date_prompt)
     
-    if (cleaning_code_date == "" | !grepl("-", cleaning_code_date) | nchar(cleaning_code_date) != 10) {
-      cat("You must provide a response in the requested format! Press ESC to stop.\n")
+    if (!grepl("-", cleaning_code_date) | nchar(cleaning_code_date) != 10) {
+      cat("You must provide a response in the requested format! Press ESC to stop.")
     } else break
   }
   
-  cat("Valid 'cleaning_code_date' entered: ", cleaning_code_date, "\n")
-  cat("Date running script ('system_date'):", system_date, "\n\n")
+  cat("Valid 'cleaning_code_date' entered:  ", cleaning_code_date, "\n",
+      "Date running script ('system_date'): ", system_date, "\n\n",
+      sep = "")
   
   ## Define versioned folder name, prepend version info to staged filenames, and 
   ## define versioned README filename
@@ -57,16 +54,17 @@ create_data_release <- function(clean_data_staging_dir, clean_data_final_dir, st
   
   ## Tell user what folder will be created and what files it will contain
   
-  cat(paste0("A folder named '", folder_name, "' will be created in:\n", 
-             clean_data_final_dir, "\n\n", 
-             "Containing these clean data files (named per 'system_date'):\n", 
-             paste(names(staged_files), collapse = "\n"), "\n\n",
-             "And this README noting the 'cleaning_code_date' and other info:\n",
-             readme_name))
+  cat("A folder named '", folder_name, "' will be created in:\n", 
+      clean_data_final_dir, "\n\n",
+      
+      "Containing these clean data files (named per 'system_date'):\n", 
+      paste(names(staged_files), collapse = "\n"), "\n\n",
+      
+      "And this README noting the 'cleaning_code_date' and other info:\n",
+      readme_name,
+      sep = "")
   
   ## Obtain user's confirmation via console
-  
-  ok <- ""
   
   repeat {
     ok <- readline("Enter y to create folder and save files or press ESC to stop: ")
@@ -90,34 +88,37 @@ create_data_release <- function(clean_data_staging_dir, clean_data_final_dir, st
   
   sink(file = paste0(clean_data_final_folder_dir, readme_name))
   
-  cat("Clean Data for Project Track-to-Treat\n")
-  cat("Contributors: Isaac Ahuvia, Jeremy Eberle, Alyssa Gorkin\n\n")
-  
-  cat("This folder, the following clean data files it contains, and this README",
-      "were created by running the cleaning code on the GitHub repository below\n", sep = "\n")
-  
-  cat("Repository URL and README: https://github.com/isaacahuvia/track-to-treat", "\n\n")
-  
-  cat("The code as of ", cleaning_code_date, " was run on ", system_date, " by the person below,\n",
-      "who assigned the following version number\n\n", sep = "")
-  
-  cat("Version:   ", version_firstname, "\n")
-  cat("Created By:", "TODO", "\n\n")
-  
-  cat("Folder:\n", clean_data_final_folder_dir, "\n\n", sep = "")
-  
-  cat("Clean Data Files:\n")
-  cat(paste(names(staged_files), collapse = "\n"), "\n\n")
-  
-  cat("Review the GitHub README for details and flags before running any analysis,",
-      "and use the version number above to indicate which clean data you analyzed\n", sep = "\n")
-  
-  cat("DO NOT modify/delete files in this folder. They may be inputs to an analysis!")
+  cat("Clean Data for Project Track-to-Treat\n",
+      "Contributors: Isaac Ahuvia, Jeremy Eberle, Alyssa Gorkin\n\n",
+      
+      "This folder, the following clean data files it contains, and this README\n",
+      "were created by running the cleaning code on the GitHub repository below\n\n",
+      
+      "Repository URL and README: https://github.com/isaacahuvia/track-to-treat\n\n",
+      
+      "The code as of ", cleaning_code_date, " was run on ", system_date, " by the person below,\n",
+      "who assigned the following version number\n\n",
+      
+      "Version:    ", version_firstname, "\n",
+      "Created By: ", "TODO", "\n\n",
+      
+      "Folder:\n",
+      clean_data_final_folder_dir, "\n\n",
+      
+      "Clean Data Files:\n",
+      paste(names(staged_files), collapse = "\n"), "\n\n",
+      
+      "Review the GitHub README for details and flags before running any analysis,\n",
+      "and use the version number above to indicate which clean data you analyzed\n\n",
+      
+      "DO NOT modify/delete files in this folder. They may be inputs to an analysis!",
+      sep = "")
   
   sink()
   
   ## Tell user path to saved files
   
-  cat(paste0("Path to saved files:\n",
-             clean_data_final_folder_dir))
+  cat("Path to saved files:\n",
+      clean_data_final_folder_dir,
+      sep = "")
 }
--- Phase 1/4_Create Clean Data Release.R
+++ Phase 1/4_Create Clean Data Release.R
@@ -26,23 +26,22 @@ clean_data_final_dir <- clean_data_dir %+% "final_read_only\\"
 
 
 ####  Instructions  ####
-# Running the function below will prompt you to provide info via the console 
-# in order to save versioned copies of the clean data into a versioned folder
+# Running the function below will prompt you to provide this info via the console 
+# in order to save versioned copies of the clean data into a versioned folder:
 
-# 1. You will first be asked to enter a version number and your first name (e.g., v0.1_Jeremy).
-#    - Use numbers < 1 (e.g., 0.1) for development versions and numbers >= 1.0 for versions suitable
-#      for analysis. When creating a new version, describe it in the README.md of this repository.
+# 1. Version number and your first name (e.g., v0.1_Jeremy)
+#    - Use numbers < 1 (e.g., 0.1) for development versions and numbers >= 1.0 for 
+#      versions suitable for analysis (describe versions in README.md of this repo)
 
-# 2. You will next be asked to enter the date the cleaning code was last modified (e.g., YYYY-MM-DD).
-#    The date you run this script (which may be different) will be auto-obtained from your system.
+# 2. Date the cleaning code was last modified (e.g., YYYY-MM-DD)
+#    - The date you run this script (may differ) will be obtained from your system
 
-# 3. You will then be asked to confirm that you would like to use this info to:
-#    - Create a versioned folder in the "final_read_only" directory (e.g., "YYYY-MM-DD_v0.1_Jeremy"),
-#      where the date is the date you ran this script
-#    - Save copies of files from the "staging" directory to the versioned folder, prepending the 
-#      version info to filenames (e.g., "YYYY-MM-DD_v0.1_Jeremy_Phase 1 LifePak Clean Data.rds"),
-#      again where the date is the date you ran this script
-#    - Save a README to the versioned folder with version info and a URL to this repository
+# 3. Confirmation that you would like to use this info to:
+#    - Create a folder in "final_read_only" directory (e.g., "YYYY-MM-DD_v0.1_Jeremy")
+#      named with date you ran this script
+#    - Copy files from "staging" directory to this folder, prepending version info 
+#      to filenames (e.g., "YYYY-MM-DD_v0.1_Jeremy_Phase 1 LifePak Clean Data.rds")
+#    - Save a README.txt to the folder with version info and a URL to this repo
 
 # DO NOT modify or delete files in the "final_read_only" directory as they may be
 # inputs to certain analysis pipelines!
