62f377e76667e757fc095493db0e419b274d9027
Isaac Ahuvia <isaacahuvia@gmail.com>
2025-05-11 16:50:00 -0400

    simplify code to calculate item completion rate (output doesn't change)

--- Qualtrics Data Cleaning Helper Functions.R
+++ Qualtrics Data Cleaning Helper Functions.R
@@ -93,54 +93,42 @@ identify_duplicates <- function(data, id) {
 # Function to compute item completion rate
 compute_item_completion_rate <- function(data, survey_prefix) {
   
-  # Define columns to ignore when computing completion rate
-  # Columns with click and time on page information
-  time_cols <- names(data)[grepl("time", names(data)) & grepl("Click|Submit", names(data))]
+  # Metadata columns
+  qualtrics_metadata <- c(
+    "StartDate", "EndDate", "Status", "IPAddress", "Progress", "Duration (in seconds)", 
+    "Finished", "RecordedDate", "ResponseId", "RecipientLastName", "RecipientFirstName", 
+    "RecipientEmail", "ExternalReference", "LocationLatitude", "LocationLongitude", 
+    "DistributionChannel", "UserLanguage", "status", "SC0"
+  )
   
-  # Columns with specified responses for response options of "Other" (or similar)
-  text_cols <- names(data)[grepl("_TEXT", names(data))]
+  survey_metadata <- c(
+    "administration", "assent_signature", "consent_signature", "p3m_address", 
+    "p3m_child_name", "p3m_childcell", "p3m_childemail", "p3m_date", "p3m_homephone",
+    "p3m_lsmh_id", "p3m_lsmh_id_validate", "p3m_parentcell", "p3m_parentemail",
+    "p3m_workphone", "p3m_wrapup_optin", "password_child", "password_parent", 
+    "pb_address", "pb_child_name", "pb_childcell", "pb_childemail", "pb_date",
+    "pb_homephone", "pb_interview", "pb_lsmh_id", "pb_lsmh_id_validate", "pb_parentcell", 
+    "pb_parentemail", "pb_workphone", "y3_childname", "y3_lsmh_id_ validate",
+    "y3m_chrome_browser", "y3m_lsmh_id", "yb_end", "yb_end_3", "yb_interview",
+    "yb_LifePak ID", "yb_LifePak ID Verify", "yb_lsmh_id", "yb_lsmh_id_ validate", 
+    "yb_phone", "yb_phone_validate"
+  )
   
-  # Columns for metadata
-  meta_cols <- c("StartDate", "EndDate", "Status", "IPAddress", "Progress", 
-                 "Duration (in seconds)", "Finished", "RecordedDate", "ResponseId", 
-                 "RecipientLastName", "RecipientFirstName", "RecipientEmail", 
-                 "ExternalReference", "LocationLatitude", "LocationLongitude", 
-                 "DistributionChannel", "UserLanguage")
+  # Remove columns that should not be included in calculation
+  data_for_calculation <- data %>%
+    select(
+      -matches("time.*(Click|Submit)"),
+      -matches("_TEXT"),
+      -any_of(qualtrics_metadata),
+      -any_of(survey_metadata)
+    )
   
-  y_meta_cols <- c("status", "SC0")
-  
-  if (survey_prefix == "yb") {
-    meta_cols <- c(meta_cols, y_meta_cols,
-                   "administration", "assent_signature", "yb_lsmh_id", "yb_lsmh_id_ validate", 
-                   "yb_phone", "yb_phone_validate", "yb_LifePak ID", "yb_LifePak ID Verify", 
-                   "yb_end", "yb_end_3", "password_child", "yb_interview")
-  } else if (survey_prefix == "y3m") {
-    meta_cols <- c(meta_cols, y_meta_cols,
-                   "y3m_lsmh_id", "y3_lsmh_id_ validate", "y3_childname", "y3m_chrome_browser")
-  } else if (survey_prefix == "pb") {
-    meta_cols <- c(meta_cols,
-                   "administration", "consent_signature", "pb_lsmh_id", "pb_lsmh_id_validate", 
-                   "pb_child_name", "pb_date", "pb_address", "pb_homephone",
-                   "pb_parentcell", "pb_childcell", "pb_workphone", "pb_parentemail", 
-                   "pb_childemail", "password_parent", "pb_interview")
-  } else if (survey_prefix == "p3m") {
-    meta_cols <- c(meta_cols,
-                   "p3m_lsmh_id", "p3m_lsmh_id_validate", 
-                   "p3m_child_name", "p3m_date", "p3m_address", "p3m_homephone", 
-                   "p3m_parentcell", "p3m_childcell", "p3m_workphone", "p3m_parentemail", 
-                   "p3m_childemail", "p3m_wrapup_optin")
-  }
-  
-  ignore_cols <- c(meta_cols, time_cols, text_cols)
-  
-  # Compute completion rate
-  item_cols <- names(data)[!(names(data) %in% ignore_cols)]
-  
-  data$item_completion_rate <- rowMeans(!is.na(data[, item_cols]))
+  # Calculate item completion rate
+  data$item_completion_rate <- rowMeans(!is.na(data_for_calculation))
   
   # Print and log items used to compute completion rate in list stored in global environment
-  cat("'item_completion_rate' for '", survey_prefix, "' survey is based on these items:\n\n", sep = "")
-  print(item_cols)
+  print("Item completion rate based on these items:")
+  print(colnames(data_for_calculation))
   
   log$item_completion_rate[[survey_prefix]]$items   <<- item_cols
   log$item_completion_rate[[survey_prefix]]$n_items <<- length(item_cols)
