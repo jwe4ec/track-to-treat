20ee97f70b37ef534121eed121fbd5eaa0506749
Isaac Ahuvia <isaacahuvia@gmail.com>
2025-06-18 13:20:53 -0400

    add scripts to load (but not yet clean) parent and youth 6m, 12m, and 18m surveys

---
+++ Phase 2/2_Clean Youth Qualtrics Data/5_Youth 12m.R
@@ -0,0 +1,31 @@
+## Track-to-Treat Phase 2 Data Cleaning, Youth Qualtrics, 3-Month Follow-Up
+# R version 4.4.3
+
+####  Startup  ####
+## Load packages
+library(groundhog) # 3.2.2
+groundhog_date <- "2025-03-28"
+meta.groundhog(groundhog_date)
+groundhog.library(
+  pkg = c("tidyverse", "tidylog", "lubridate", "qualtRics", "openxlsx", "here"),
+  date = groundhog_date
+)
+`%+%` <- paste0
+
+
+## Load helper functions
+source(here("Qualtrics Data Cleaning Helper Functions.R"))
+source(here("Version Control Helper Functions.R"))
+
+
+## Load Qualtrics data
+# Save directories
+raw_data_dir <- "R:\\MSS\\Schleider_Lab\\jslab\\TRACK to TREAT P2\\Data\\Qualtrics\\Raw\\2025.05.22_interim\\"
+clean_data_dir <- "R:\\MSS\\Schleider_Lab\\jslab\\TRACK to TREAT P2\\Data\\Clean Data (Isaac)\\"
+clean_data_staging_dir <- clean_data_dir %+% "staging\\"
+clean_data_staging_intermediate_dir <- clean_data_staging_dir %+% "intermediate\\"
+
+# Load raw Qualtrics datasets (storing paths) in this format: [respondent][wave]_[administration]_raw
+# - Note: Use "timeZone" specified for date columns (e.g., "StartDate") in third row of raw CSV
+y12m_path <- raw_data_dir %+% "DP5+Phase+2+-+Youth+-+FU+3+-+12M_May+6,+2025_09.49_n.csv"
+y12m_raw <- read_survey(y12m_path, time_zone = "America/Denver")
